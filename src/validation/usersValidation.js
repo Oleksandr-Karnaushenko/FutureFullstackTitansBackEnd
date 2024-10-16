91ac082ba468c32bd79fbcc2197575de75d21573
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-10-16 21:20:50 +0200

    fix-commit

--- src/validation/usersValidation.js
+++ src/validation/usersValidation.js
@@ -1,10 +1,6 @@
 
 import Joi from 'joi';
 
-//   gender: Joi.string().valid('male', 'female').optional().messages({
-//     'any.only': '"Gender" must be one of "male", "female"',
-//   }),
-
 export const userRegistrationValidation = Joi.object({
   email: Joi.string().email().required().messages({
     'string.email': '"Email" must be a valid email address',
@@ -35,11 +31,24 @@ export const avatarUserUrlValidation = Joi.object({
      }),
 })
 
-export const updateUserValidation = Joi.object({
+export const updateUserDataValidation = Joi.object({
+  name: Joi.string().min(2).max(32).optional().messages({
+    'string.min': '"Name" must be at least 2 characters long',
+    'string.max': '"Name" cannot be longer than 100 characters',
+  }),
   email: Joi.string().email().optional().messages({
     'string.email': '"Email" must be a valid email address',
   }),
+  password: Joi.string().min(8).max(64).required().messages({
+    'string.min': '"Password" must be at least 8 characters long',
+    'string.max': '"Password" cannot be longer than 64 characters',
+    'any.required': '"Password" is required',
+  }),
+  gender: Joi.string().valid('male', 'female').optional().messages({
+    'any.only': '"Gender" must be one of "male", "female"',
+  }),
 });
+
 
 export const updateUserWaterIntakeValidation = Joi.object({
   dailyNorm: Joi.number().min(0).max(15000).required().messages({
