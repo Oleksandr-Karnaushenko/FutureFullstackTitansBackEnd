394c6f908f7f8d79035484e13727bb75b3b87257
Ihor Matviienko <igor.matvienko1989@gmail.com>
2024-10-23 20:22:14 +0300

    update

--- src/middlewares/checkAccessToUsersData.js
+++ src/middlewares/checkAccessToUsersData.js
@@ -1,11 +1,13 @@
-import createHttpError from "http-errors";
+import createHttpError from 'http-errors';
 
 const checkAccessToUsersData = (req, res, next) => {
-    const {userId} = req.params;
-    if(req.user._id.toString() !== userId) {
-      return next(createHttpError(403, 'Access forbidden'))
-    }
-    next();
-}
+  const { id, userId } = req.params;
+  const reqId = userId || id;
+
+  if (req.user._id.toString() !== reqId) {
+    return next(createHttpError(403, 'Access forbidden'));
+  }
+  next();
+};
 
 export default checkAccessToUsersData;
